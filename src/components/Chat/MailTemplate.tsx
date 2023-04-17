34385faf6f462b1503bb07812e00a4b2e2341361
NavasardianMichael <navasardianmichael@gmail.com>
2023-04-18 01:34:33 +0400

    fixed wrong word in customer support email

--- src/components/Chat/MailTemplate.tsx
+++ src/components/Chat/MailTemplate.tsx
@@ -15,7 +15,7 @@ const style: React.CSSProperties = {
 export const MailTemplate: FC<T_Props> = ({values}) => {
     return (
         <div>
-            <h1 style={{textAlign: 'center'}}>Сообщение от пожалователя в службу поддержки SAV</h1>
+            <h1 style={{textAlign: 'center'}}>Сообщение от пользователя в службу поддержки SAV</h1>
             <img src='https://cdn.templates.unlayer.com/assets/1681100988650-Asset%201.png' style={style} />
             {
                 CONTACT_FORM_TEMPLATE.map(field => {
