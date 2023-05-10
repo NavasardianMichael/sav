aa6f75288b928e33bf3298cf84306074f4b435a5
Michael Navasardyan <navasardianmichael@gmail.com>
2023-05-10 16:32:14 +0400

    added new logo svg and how to order sectino text is fixed

--- src/components/OrderInstructions/Main.tsx
+++ src/components/OrderInstructions/Main.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.scss';
 export const OrderInstructions: FC = () => {
     return (
         <Section id={HOME_PAGE_SECTIONS.byId.orderInstructions.id} className={styles.orderInstructions}>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, autem! Reiciendis debitis velit, suscipit laudantium at ex alias ea, magnam veritatis aut aliquid, enim delectus consequuntur facilis eveniet! Accusamus iure impedit ab. Doloremque suscipit consectetur et. Dignissimos magni, minus temporibus debitis possimus doloremque dolor dicta velit impedit architecto, ipsum est nesciunt! Natus labore qui ratione exercitationem eaque ad suscipit quibusdam reprehenderit quidem! Recusandae illo eaque nisi iste beatae cumque dicta dolore quibusdam eveniet quaerat, sunt maxime suscipit? Aut expedita temporibus architecto rem quidem blanditiis mollitia exercitationem. In accusamus sint doloribus harum ea, assumenda, suscipit praesentium, nisi hic ipsam quasi iure?</p>
+            <p>Для того, чтобы оформить заказ, добавьте необходимые товары в корзину и заполните форму обратной связи. В течение часа после оформления заказа с Вами свяжется менеджер для согласования индивидуальной скидки и дальнейшего оформления доставки.</p>
         </Section>
     )
 }
